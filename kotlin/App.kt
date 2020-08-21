20e52927be13d638941c2f10723e454777154b77
Denis09-max <69382164+Denis09-max@users.noreply.github.com>
2020-08-21 14:20:21 +0700

    Add files via upload

---
+++ kotlin/App.kt
@@ -0,0 +1,9 @@
+/*
+Nama  : Denis Rimbawan
+Kelas : XI RPL 3
+No    : 09
+ */
+
+fun main(){
+    println("Hello kotlin")
+}
---
+++ kotlin/Hello Kotlin.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var name = "Denis Rimbawan"
+    var hobby = "Photography"
+
+    print("Nama saya ");
+    println(name);
+    print("Hobby saya ");
+    println(hobby)
+}
